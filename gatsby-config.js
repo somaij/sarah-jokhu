0a50abf362fd241b2f537d5972212482dcc2ebfa
Jason Somai <jsomai458@gmail.com>
2022-01-19 21:14:03 -0500

    fixing build

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
-      "path": "./public/static/assets/"
+      "path": "public/static/assets/"
     },
     __key: "images"
   },{
