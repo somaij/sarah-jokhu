aab3802bedf890379f10deda671535e1d4774413
Jason Somai <jsomai458@gmail.com>
2022-01-21 00:31:48 -0500

    fixing reference

--- src/components/templates/BlogPost.js
+++ src/components/templates/BlogPost.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../layout"
 import Img from "gatsby-image"
 import { Helmet } from "react-helmet"
 
